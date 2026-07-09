917c6cab91ec3cb118c807c87591c6c0466157ed
Mohammad Polaei <mohammadpolaei@gmail.com>
2026-07-09 17:32:31 +0330

    feat : forecast carts created

--- src/features/predictions/prediction-card.tsx
+++ src/features/predictions/prediction-card.tsx
@@ -1,10 +1,33 @@
 import SectionContainer from "@/components/base/section-container";
+import { Coins } from "lucide-react";
+import { JSX } from "react/jsx-runtime";
 import PredictionOptions from "./prediction-options";
 import PredictionProgress from "./prediction-progress";
 
-export default function PredictionCard() {
+export default function PredictionCard({ title }: { title: JSX.Element }) {
 	return (
-		<SectionContainer>
+		<SectionContainer extraClass="w-full p-4">
+			<div className="w-full flex justify-between items-center gap-44">
+				<SectionContainer
+					extraClass="flex justify-start items-center gap-1 max-w-25 py-1"
+					rounded="rounded-[7px]"
+				>
+					<Coins size={10} className="text-(--text-muted)" />
+					<span className="text-[10px] text-(--text-muted) font-bold">
+						هزینه 0 امتیاز
+					</span>
+				</SectionContainer>
+				<SectionContainer
+					extraClass="flex justify-start items-center gap-1 max-w-25 py-1"
+					rounded="rounded-[7px]"
+				>
+					<Coins size={10} className="text-(--text-main)" />
+					<span className="text-[10px] text-(--text-main)">352</span>
+				</SectionContainer>
+			</div>
+			<div className="">
+				<h3>{title}</h3>
+			</div>
 			<PredictionOptions />
 			<PredictionProgress />
 		</SectionContainer>
--- src/features/predictions/prediction-section.tsx
+++ src/features/predictions/prediction-section.tsx
@@ -2,8 +2,20 @@ import PredictionCard from "./prediction-card";
 
 export default function PredictionSection() {
 	return (
-		<div>
+		<div className="w-full flex flex-col justify-start items-center gap-3">
+			<PredictionCard
+				title={
+					<span className="font-bold text-[14px]">
+						این بازی چند کارت <span className="text-yellow-400">زرد</span> دارد
+						؟
+					</span>
+				}
+			/>
+			{/* <PredictionCard />
 			<PredictionCard />
+			<PredictionCard />
+			<PredictionCard />
+			<PredictionCard /> */}
 		</div>
 	);
 }
