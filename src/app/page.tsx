1c95bc5a2d8c7ef777d04d6337b208a7b8ed955d
Mohammad Polaei <mohammadpolaei@gmail.com>
2026-07-11 12:55:11 +0330

    feat : initial header UI created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ import MobileNavbar from "@/layout/mobile-navbar";
 
 export default function Home() {
 	return (
-		<div className="min-h-screen max-w-93.75 bg-app-bg pb-24 flex flex-col justify-start items-center">
+		<div className="min-h-screen max-w-93.75 bg-app-bg pb-24 flex flex-col justify-start items-center relative">
 			<Header />
-			<main className="container mx-auto px-4 py-4 space-y-3">
+			<main className="container mx-auto px-4 py-4 space-y-3 pt-18">
 				<MatchHeroCard />
 				<PredictionSection />
 				<ThisMatchScore />
--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import { CircleChevronRight } from "lucide-react";
+
 export default function Header() {
-	return <div>header</div>;
+	return (
+		<div className="w-full py-5 px-5 fixed z-100 backdrop-blur-[3px] shadow-sm bg-(--text-muted)/5">
+			<button
+				onClick={() => {
+					//back function
+				}}
+				className="flex items-center gap-1"
+			>
+				<CircleChevronRight size={25} className="text-(--primary)" />
+				<span className="text-[14px] font-bold">بازگشت</span>
+			</button>
+			<div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	);
 }
